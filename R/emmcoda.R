b857c243b0886ca226e15293cb004519d6cbba39
Flora Le <flora.le@monash.edu>
2022-04-27 14:43:26 +1000

    updated functions for compositional outcome [ci skip]

--- R/emmcoda.R
+++ R/emmcoda.R
@@ -3,48 +3,133 @@
 #' @description
 #' This function obtains estimated marginal means (EMMs) of models
 #' containing composition as the outcome
+#' A sample from the posterior distribution of the regression coefficients.
 #'
-#' @param data A fitted \code{\link{mvmcoda}} model data. Required.
-#' @param specs A character vector specifying the names of the predictors over which EMMs are desired.
-#' @param at A numeric vector passed to \code{\link{ref_grid}} specifying the predictor's values for EMMs.
-#' @param ... Further arguments passed to \code{\link{emmeans}}.
+#' @param object A fitted \code{\link{mvmcoda}} model data. Required.
+#' @param x A character vector specifying the names of the predictors over which EMMs are desired.
+#' If \code{NULL}, the average of all predictionu is returned. Default to \code{NULL}.
+#' @param at A numeric value, vector or list passed to \code{\link{ref_grid}}
+#' specifying the predictor's values for EMMs.
+#' @param ... Additional arguments to be passed to \code{\link{describe_posterior}}.
 #'
 #' @return A list with four elements.
 #' @importFrom compositions ilrInv
 #' @importFrom emmeans emmeans
-#' @importFrom data.table as.data.table
-#' @importFrom reshape2 melt dcast
+#' @importFrom data.table as.data.table setDT rbindlist
+#' @importFrom reshape2 dcast
+#' @importFrom stats predict
 #' @export
 #' @examples
+#' data("unadjusted-mvmcoda")
 #' 
-#' emtest <- emmcoda(object = mvm1, specs = "STRESS", at = c(1:10))
-#'
-emmcoda <- function (object, specs, at, ...) {
+#' # emtest <- emmcoda(object = mvm, x = "STRESS", at = 2)
+emmcoda <- function (object, x = NULL, at = NULL, ...) {
+  
+  if(isTRUE(missing(object))) {
+    stop(paste(
+      "argument 'object' is missing, with no default",
+      "  it should be an object of class mvmcoda.",
+      "  See ?emmcoda for details.",
+      sep = "\n"))
+  }
 
-  tmp <- copy(object)
+  if(isFALSE(inherits(object, "mvmcoda"))) {
+    stop(sprintf(
+    "Can't handle an object of class (%d)
+  object should be a fitted 'mvmcoda' object
+  See ?emmcoda for details.",
+                 class(object)))
+  }
   
-  ls <- list(at)
-  names(ls) <- specs    
+  # extract variable information from model
+  vn <- rbindlist(find_predictors(object$Model)) # get all varnames in brm model
+  vn <- unique(vn)
+  rg <- as.data.table(ref_grid(object$Model) @grid)
+  
+  if(isFALSE(is.null(at)) && is.null(x)) {
+      stop("'at' is level of x, but 'x' is missing")
+    
+      } else if (is.null(at) && is.null(x)) {
+        # no user's specified value - use default ref grid
+        cv <- colnames(rg) %snin% c("rep.meas", ".wgt.") # get cov names, including x
+        rg <- rg[, cv, with = FALSE]
+        rg <- unique(rg)
+        newd <- rg[, ID := 1]
+        
+        } else if (is.null(at) && isFALSE(is.null(x))) {
+          if(isFALSE(x %in% colnames(rg))) { # check name
+            stop("'x' is not a predictor in 'mvmcoda' object")
+          } else {
+            cv <- colnames(rg) %snin% c("rep.meas", ".wgt.", x) # get cov names, without x
+            rg <- rg[, cv, with = FALSE]
+            rg <- unique(rg)
+            newd <- rg[, ID := 1]
+          }
+          
+          } else { # both x and at are provided
+            if(isFALSE(x %in% colnames(rg))) { 
+              stop("'x' is not a predictor in 'mvmcoda' object")
+            } else if (isFALSE(identical(class(at), class(rg[, get(x)])))) {
+              stop(sprintf(
+                "at (%s) must be values of x (%s)",
+                class(at),
+                class(rg[, get(x)])))
+              } else {
+                cv <- colnames(rg) %snin% c("rep.meas", ".wgt.", x)
+                rg <- rg[, cv, with = FALSE]
+                rg <- unique(rg)
+                ls <- list(at)
+                names(ls) <- x
+                newd <- setDT(ls)[, ID := 1]
+                
+                iout <- vector("list", length(at))
+                if (isFALSE(nrow(vn) == 1)) {
+                  for (i in seq_len(nrow(rg))) {
+                    nd <- cbind(newd, rg[i, ])
+                    iout[[i]] <- nd
+                    }
+                  newd <- do.call(rbind, iout)
+                }
+              }
+          }
 
-  em <- emmeans(tmp$BrmModel, pairwise ~ ilr, specs, mult.name = "ilr", at = ls, ...)
-  emd <- as.data.table(em$emmeans)
+  yhat <- as.data.table(predict(object$Model, newdata = newd, re.form = NA, summary = FALSE))
+  yhat <- dcast(yhat, V1 + V2 ~ V3, value.var = "value") # V1 - posterior draws, V2 - row in newd, V3 - ilr
+  yhat <- yhat[order(yhat$V2),] # yhat should have already been sorted by newd, but just to be sure
   
-  ilr <- emd[, 1:3]
-  ilr <- spread(ilr, key = "ilr", value = "emmean")
+  # inverse ilr to get composition
+  ilr <- yhat[, -c(1:2)]
   
-  ilrinv <- ilrInv(ilr[, 2:ncol(ilr)], V = tmp$CompIlr$psi)
-  ilrinv <- clo(ilrinv, total = 1440)
-  ilrinv <- as.data.table(ilrinv)
-  ilrinv <- cbind(ilr[, 1], ilrinv)
-  colnames(ilrinv) <- c(specs, tmp$CompIlr$composition)
-  
-  ilrinvl <- melt(ilrinv, id = specs)
-  
-  out <- list(
-    emmILR = emd,
-    emmWide = ilrinv,
-    emmLong = ilrinvl,
-    Predictor = specs)
-  
-  return(out)
+  if(is.null(x)) { # average all prediction
+    comp <- ilrInv(ilr, V = object$CompIlr$psi)
+    comp <- as.data.table(clo(comp, total = object$CompIlr$total))
+    names(comp) <- object$CompIlr$parts
+    comp <- describe_posterior(comp, centrality = "mean")
+
+    ilr <- describe_posterior(ilr, centrality = "mean")
+    
+  } else {
+    ilr <- split(ilr, rep(1:nrow(newd), each = nrow(yhat) / nrow(newd)))
+    
+    comp <- lapply(ilr, function(x) {
+      x <- ilrInv(x, V = object$CompIlr$psi)
+      x <- as.data.table(clo(x, total = object$CompIlr$total))
+      x <- setNames(x, object$CompIlr$parts)
+      x <- describe_posterior(x, centrality = "mean")})
+    ilr <- lapply(ilr, function(x) {describe_posterior(x, centrality = "mean")})
+    
+    d <- split(newd, rep(1:nrow(newd), each = 1))
+    
+    dcomp <- lapply(d, function(x){x[rep(seq_len(nrow(x)), length(object$CompIlr$parts)), ]})
+    dilr <- lapply(d, function(x){x[rep(seq_len(nrow(x)), ncol(object$CompIlr$TotalILR)), ]})
+    
+    comp <- Map(cbind, comp, dcomp)
+    ilr <- Map(cbind, ilr, dilr)
+    
+    names(comp) <- paste0(x, "-", at)
+    names(ilr) <- paste0(x, "-", at)
+  }
+  out <- list(comp, ilr)
+  names(out) <- c("Composition", "ILR")
+  out
 }
--- R/mvmcoda.R
+++ R/mvmcoda.R
@@ -1,44 +1,57 @@
 #' Fit Bayesian generalised (non-)linear multivariate multilevel compositional model 
-#' via full Bayesian inference using Stan,
+#' via full Bayesian inference using brms,
 #' when composition is the outcome.
 #'
-#'
-#' This function utilises 
 #' @param formula A object of class \code{\link{brmsformula}}, \code{\link{mvbrmsformula}}:
 #' A symbolic description of the model to be fitted. 
 #' Details of the model specification can be found in \code{\link{mvbrmsformula}}.
-#' @param compilr A \code{compilr} object containing data of composition, ILR coordinates,
-#' and other variables used in the model.
+#' @param compilr A \code{\link{compilr}} object containing data of composition, 
+#' ILR coordinates, and other variables used in the model.
 #' @param ... Further arguments passed to \code{\link{brm}}.
 #' 
 #' @return 
 #' @importFrom brms brm
 #' @export
 #' @examples
+#' \donttest{
 #' data(mcompd)
 #' data(sbp)
-#' cilr <- compilr(data = mcompd, sbp = sbp)
+#' cilr <- compilr(data = mcompd, sbp = sbp, idvar = "ID",
+#'                 parts = c("TST", "WAKE", "MVPA", "LPA", "SB"))
 #' 
 #' ## inspect names of ILR coordinates bfore passing to 'brm' model
 #' names(cilr$TotalILR)
 #' 
 #' ## run mvmcoda
 #' mvm <- mvmcoda(compilr = cilr, formula = mvbind(ilr1, ilr2, ilr3, ilr4) ~ STRESS + (1 | ID),
-#'                 chains = 4, cores = 8)
-#' 
+#'                cores = 8, chain = 4)
+#' }
 mvmcoda <- function(compilr, formula, ...) {
   
-  message("Please be patient, mvmcoda is working hard...")
+  if (isTRUE(missing(compilr))) {
+    stop(paste(
+      "'compilr' is a required argument and cannot be missing;",
+      " it should be an object of class compilr.", 
+      " See ?multilevelcoda::compilr for details.",
+      sep = "\n"))
+  }
+  
+  if (isFALSE(inherits(compilr, "compilr"))) {
+    stop(paste(
+      "compilr must be an object of class compilr.",
+      " See ?multilevelcoda::compilr for details.",
+      sep = "\n"))
+  }
+  message("Please be patient, 'mvmcoda' is working hard...")
 
   tmpd <- cbind(compilr$data, compilr$TotalILR)
-  
-  m <- brm(eval(formula),
+  m <- brm(formula,
            data = tmpd,
            ...)
   
-  out <- list(
-    CompIlr = compilr,
-    BrmModel = m)
-  
-  return(out)
+  out <- structure(
+    list(CompIlr = compilr,
+         BrmModel = m),
+    class = "mvmcoda")
+  out
 }
