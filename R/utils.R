ead3af69bb281fbda60a398a378c444026bc9a27
Flora Le <flora.le@monash.edu>
2022-09-16 11:04:53 +1000

    Revert "move utilities to separate script"
    This reverts commit 8f8acb9dcf433b4ab25bf10ce02af6b00aecb381.

--- R/utils.R
+++
@@ -1,4 +0,0 @@
-## make Rcmd check happy
-utils::globalVariables(c("i",  "..cols", ".", "Predictor", ".SD",
-                         "Mean",  "CI_low", "CI_high", "Substitute", "MinSubstituted",
-                         "spread", "value", "variable", "ID"))
