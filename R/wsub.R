e781a835b3ca0ba68896f0a54b91ee3aa50f5ea8
Flora Le <flora.le@monash.edu>
2022-10-08 20:25:55 +1100

    Revert "remove computing mean using robustness "
    This reverts commit 30279f0375cd9f74f666b49f4f99f3dc00681c24.

--- R/wsub.R
+++ R/wsub.R
@@ -120,7 +120,7 @@ wsub <- function(object, substitute, minute = 60L,
   # compositional mean
   b <- object$CompIlr$BetweenComp
   
-  mcomp <- mean.acomp(b)
+  mcomp <- mean(b, robust = TRUE)
   mcomp <- clo(mcomp, total = object$CompIlr$total)
   mcomp <- as.data.table(t(mcomp))
 
