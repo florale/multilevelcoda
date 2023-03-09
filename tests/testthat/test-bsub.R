a1f64a635cc257c1f1bce96208d0bc6d55572278
Flora Le <flora.le@monash.edu>
2023-03-09 18:15:18 +1100

    GHA - fix typo in testthat

--- tests/testthat/test-bsub.R
+++ tests/testthat/test-bsub.R
@@ -12,7 +12,7 @@ if (!requireNamespace("cmdstanr", quietly = TRUE)) {
 }
 
 if(identical(backend, "cmdstanr")) {
-  expect_true(requiredNamespace("cmdstanr"))
+  expect_true(requireNamespace("cmdstanr"))
 }
 
 # Packages
@@ -235,7 +235,7 @@ test_that("bsub gives results in sensible range", {
   expect_true(x$LPA$Mean %ae% "[-0.5, 0) | (0, 0.5]")
   expect_true(x$SB$Mean %ae% "[-0.5, 0) | (0, 0.5]")
   
-  expect_true(x$TST$CI_low %ae% "[-1, 1]")
+  expect_true(x$TST$CI_low %ae% "[-1, 0) | (0, 1]")
   expect_true(x$WAKE$CI_low %ae% "[-1, 0) | (0, 1]")
   expect_true(x$MVPA$CI_low %ae% "[-1, 0) | (0, 1]")
   expect_true(x$LPA$CI_low %ae% "[-1, 0) | (0, 1]")
