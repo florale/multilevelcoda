f0040bfd1a013e77ce3c26a783076033907ca5c3
Flora Le <flora.le@monash.edu>
2023-07-20 15:59:00 +1000

    shiny related function(s)

---
+++ R/shiny.R
@@ -0,0 +1,20 @@
+#' Interface to \pkg{shinystan}
+#' 
+#' Provide an interface to \pkg{shinystan} for models fitted with \pkg{brms}
+#' 
+#' @aliases launch_shinystan
+#' 
+#' @param object A fitted model object of class \code{brmcoda}.
+#' @param ... Optional arguments to pass to 
+#' \code{\link{launch_shinystan.brmsfit}} or \code{\link[shiny:runApp]{runApp}}
+#'
+#' @seealso \code{\link[shinystan:launch_shinystan]{launch_shinystan}}
+#' 
+#' @return An S4 shinystan object
+#' 
+#' @method launch_shinystan brmcoda
+#' @importFrom shinystan launch_shinystan
+#' @export
+launch_shinystan.brmcoda <- function(object, ...) {
+  launch_shinystan(object$Model, ...)
+}
