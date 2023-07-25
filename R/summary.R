34c1b2d612dd389aa24a116cd25b4c7f50ee3fba
Flora Le <flora.le@monash.edu>
2023-07-25 23:53:42 +1000

    fix a non-ASCII characters issue

--- R/summary.R
+++ R/summary.R
@@ -1,9 +1,9 @@
 #' Create a Summary of a \code{compilr} object
 #' 
 #' @param object An object of class \code{compilr}.
-#' @param class  Optional. Can be \code{"composition"} and/or \code{"logratio"} to
+#' @param class Optional. Can be \code{"composition"} and/or \code{"logratio"} to
 #' specify the geometry of the composition.
-#' @param level  Optional. Can be \code{"between"}, \code{"within"}, and/or \code{"total"}
+#' @param level Optional. Can be \code{"between"}, \code{"within"}, and/or \code{"total"}
 #' indicating the level of the geometry.
 #' @param weight A character value specifying the weight to use in calculation of the reference composition.
 #' If \code{"equal"}, give equal weight to units (e.g., individuals).
@@ -72,21 +72,21 @@ summary.compilr <- function(object,
   
   if ("composition" %in% class) {
     if ("total" %in% level) {
-      cat("\n", "———— Raw Composition Statistics ————", "\n")
+      cat("\n", " Raw Composition Statistics: ", "\n")
       for (i in seq_along(output$tcomp)) {
         cat(paste0("\n", names(output$tcomp)[i], ":", "\n"))
         print(output$tcomp[[i]], digits = digits)
       }
     }
     if ("between" %in% level) {
-      cat("\n", "——— Between-level Composition ———", "\n")
+      cat("\n", " Between-level Composition: ", "\n")
       for (i in seq_along(output$bcomp)) {
         cat(paste0("\n", names(output$bcomp)[i], ":", "\n"))
         print(output$bcomp[[i]], digits = digits)
       }
     }
     if ("within" %in% level) {
-      cat("\n", "——— Within-level Composition ———", "\n")
+      cat("\n", " Within-level Composition: ", "\n")
       for (i in seq_along(output$wcomp)) {
         cat(paste0("\n", names(output$wcomp)[i], ":", "\n"))
         print(output$wcomp[[i]], digits = digits)
@@ -96,15 +96,15 @@ summary.compilr <- function(object,
   
   if ("logratio" %in% class) {
     if ("total" %in% level) {
-      cat("\n", "———— Raw Isometric Log-ratios ————", "\n")
+      cat("\n", " Raw Isometric Log-ratios: ", "\n")
       print(output$tilr, digits = digits)
     }
     if ("between" %in% level) {
-      cat("\n", "——— Between-level Isometric Log-ratios ———", "\n")
+      cat("\n", " Between-level Isometric Log-ratios: ", "\n")
       print(output$bilr, digits = digits)
     }
     if ("within" %in% level) {
-      cat("\n", "——— Within-level Isometric Log-ratios ———", "\n")
+      cat("\n", " Within-level Isometric Log-ratios: ", "\n")
       print(output$wilr, digits = digits)
     }
   }
@@ -121,7 +121,7 @@ summary.compilr <- function(object,
 #' Print a Summary for a \code{compilr} object
 #' 
 #' @param x An object of class \code{compilr}.
-#' @param ... Other arguments passed to \code{summary.compilr}.
+#' @param ... Other arguments passed to \code{\link{summary.compilr}}.
 #' 
 #' @seealso \code{\link{summary.compilr}}
 #' 
@@ -134,7 +134,7 @@ print.compilr <- function(x, ...) {
 #' Create a Summary of a fitted \code{brmsfit} model in a \code{brmcoda} object
 #' 
 #' @param object An object of class \code{brmcoda}.
-#' @param ... Other arguments passed to \code{summary.brmsfit}.
+#' @param ... Other arguments passed to \code{\link{summary.brmsfit}}.
 #' 
 #' @method summary brmcoda
 #' @export
@@ -157,24 +157,24 @@ print.brmcoda <- function(x, ...) {
   
 }
 
-#' Create a Summary of a Substitution Model represented by a \code{\link{substitution}} object
+#' Create a Summary of a Substitution Model represented by a \code{substitution} object
 #' 
-#' @param object A \code{\link{substitution}} class object.
+#' @param object A \code{substitution} class object.
 #' @param delta A integer, numeric value or vector indicating the desired \code{delta} 
 #' at which substitution results should be summarised.
-#' Default to all \code{delta} available in the \code{\link{substitution}} object.
+#' Default to all \code{delta} available in the \code{substitution} object.
 #' @param to A character value or vector specifying the names of the compositional parts
 #' that were reallocated to in the model.
 #' @param from A character value or vector specifying the names of the compositional parts
 #' that were reallocated from in the model.
 #' @param ref Either a character value or vector ((\code{"grandmean"} and/or \code{"clustermean"} or \code{"users"}),
-#' Default to all \code{ref} available in the \code{\link{substitution}} object .
+#' Default to all \code{ref} available in the \code{substitution} object.
 #' @param level A character string or vector (\code{"between"} and/or \code{"within"}).
-#' Default to all \code{level} available in the \code{\link{substitution}} object.
+#' Default to all \code{level} available in the \code{substitution} object.
 #' @param digits A integer value used for number formatting. Default is \code{2}.
 #' @param ... generic argument, not in use.
 #' 
-#' @return A summary of \code{\link{substitution}} object.
+#' @return A summary of \code{substitution} object.
 #' \itemize{
 #'   \item{\code{Mean}}{ Posterior means.}
 #'   \item{\code{CI_low} and \code{CI_high}}{ 95% credible intervals.}
