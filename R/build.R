b1fd52b59f9dcb878956199893b00f079101ec8f
Flora Le <flora.le@monash.edu>
2024-10-02 14:47:11 +1000

    soft introduce default sbp

---
+++ R/build.R
@@ -0,0 +1,81 @@
+#' Build Base Pairwise Substitution 
+#'
+#' @description
+#' Make a data set of all possible pairwise substitution of a composition which can be used as 
+#' the base for substitution models.
+#' @param parts A character vector specifying the names of compositional variables to be used.
+#' 
+#' @return A data table of all possible pairwise substitution.
+#' @importFrom data.table as.data.table 
+#' @export
+#' @examples
+#' ps1 <- build.basesub(parts = c("TST", "WAKE", "MVPA", "LPA", "SB"))
+#' print(ps1)
+#' 
+#' ps2 <- build.basesub(c("WAKE", "MVPA", "LPA", "SB"))
+#' print(ps2)
+build.basesub <- function(parts) {
+  
+  d <- length(parts)
+  n <- d - 2
+  
+  subvars1 <- c(1, -1)
+  subvars2 <- rep(0, n)
+  subvars <- c(subvars1, subvars2)
+  
+  nc <- length(subvars)
+  nr <- (nc - 1) * d
+  
+  base_sub <- matrix(0, nrow = nr, ncol = nc, dimnames = list(NULL, parts))
+  k <- 0
+  
+  for (i in 1:nc)
+    for (j in 1:nc)
+      if (i != j) {
+        k <- k + 1
+        base_sub[k, c(i, j)] <- c(1, -1)
+      }
+  
+  base_sub <- as.data.table(base_sub)
+  base_sub
+}
+
+#' Build Sequential Binary Partition
+#'
+#' @description
+#' Build a default sequential binary partition for \code{complr} object.
+#' The default sequential binary partition is a pivot balance that allows 
+#' the effect of this first balance coordinate to be interpreted as the change 
+#' in the prediction for the dependent variable 
+#' when that given part increases while all remaining parts decrease by a common proportion.
+#' @param parts A character vector specifying the names of compositional variables to be used.
+#' 
+#' @return A matrix sequential binary partition.
+#' @importFrom data.table as.data.table 
+#' @export
+#' @examples
+#' ps1 <- build.sbp(parts = c("TST", "WAKE", "MVPA", "LPA", "SB"))
+#' print(ps1)
+#' 
+#' ps2 <- build.sbp(c("WAKE", "MVPA", "LPA", "SB"))
+#' print(ps2)
+build.sbp <- function(parts) {
+  
+  d <- length(parts)
+  k <- 0
+  
+  nc <- d
+  nr <- d - 1
+  
+  base_sbp <- matrix(NA, nrow = nr, ncol = nc, dimnames = list(NULL, parts))
+  sbp <- base_sbp
+  
+  for (i in 1:nr) {
+        # k <- i - 1
+        base_sbp[i,  i] <- 1
+        base_sbp[i, -i] <- -1
+        base_sbp[i, 0:(i-1)] <- 0
+  }
+  base_sbp
+}
+
